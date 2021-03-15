afd0e678c9bd327374c876017ed135775eb45a27
terremoth <dutr4@outlook.com>
2021-03-15 20:25:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+setTimeout(function() {
+    try {
+        document.querySelector('.R361B').firstElementChild.remove();
+    } catch(e) {}
+    
+}, 1000);
+
+setInterval(function() {
+    try {
+        document.querySelector('div.RnEpo').previousSibling.remove();
+    } catch(e) {}
+
+    document.body.style.overflow = 'auto';
+}, 700);
